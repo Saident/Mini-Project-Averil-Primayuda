d11f505baefb0cf2bb2ee67aa0eec05708df4fd0
Saident <averilprimayuda@gmail.com>
2023-05-03 11:41:18 +0700

    DB and Run update

--- Route/route.go
+++ Route/route.go
@@ -1,1 +1,10 @@
 package route
+
+import (
+	"github.com/labstack/echo"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+	return e
+}
--- server.go
+++ server.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"github.com/Saident/Mini-Project-Averil-Primayuda/route"
+)
+
+func main() {
+	e := route.New()
+	e.Logger.Fatal(e.Start(":8000"))
+}
