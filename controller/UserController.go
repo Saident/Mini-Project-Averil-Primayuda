fcf9666a6a53181cc5c96678c10634f70f090aa8
Saident <averilprimayuda@gmail.com>
2023-05-04 21:14:20 +0700

    GetUser on UserController

--- controller/UserController.go
+++ controller/UserController.go
@@ -1,1 +1,39 @@
 package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Saident/Mini-Project-Averil-Primayuda/config"
+	"github.com/Saident/Mini-Project-Averil-Primayuda/model"
+	"github.com/labstack/echo"
+)
+
+func GetUsersController(c echo.Context) error {
+	var users []model.User
+
+	if err := config.DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+}
+
+func GetUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	var users model.User
+	if err := config.DB.First(&users, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get users by id",
+		"users":   users,
+	})
+}
