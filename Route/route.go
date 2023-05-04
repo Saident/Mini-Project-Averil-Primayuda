2c310c92b57f236e48321efe9ca8fa14956d21db
Saident <averilprimayuda@gmail.com>
2023-05-04 19:31:25 +0700

    Update on Route

--- Route/route.go
+++ Route/route.go
@@ -3,11 +3,19 @@ package route
 import (
 	"github.com/labstack/echo"
 	m "github.com/Saident/Mini-Project-Averil-Primayuda/middleware"
+
+	"github.com/Saident/Mini-Project-Averil-Primayuda/constants"
+	mid "github.com/labstack/echo/middleware"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
+	//Non-JWT Route
 	
+	eJwt := e.Group("")
+	eJwt.Use(mid.JWT([]byte(constants.SECRET_JWT)))
+	//JWT Route
+
 	m.LogMiddleware(e)
 	return e
 }
