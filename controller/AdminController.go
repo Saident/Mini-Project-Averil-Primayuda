6c48afd9b70936d64fb4c4bd3879ebffd3ea49d7
Saident <averilprimayuda@gmail.com>
2023-05-05 11:41:05 +0700

    CRUD and added Validate Jobs func

--- controller/AdminController.go
+++ controller/AdminController.go
@@ -1,1 +1,80 @@
 package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Saident/Mini-Project-Averil-Primayuda/config"
+	"github.com/Saident/Mini-Project-Averil-Primayuda/model"
+	"github.com/labstack/echo"
+)
+
+func GetAdminsController(c echo.Context) error {
+	var admins []model.Admin
+
+	if err := config.DB.Find(&admins).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all admins",
+		"admins":  admins,
+	})
+}
+
+func GetAdminController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	var admins model.Admin
+	if err := config.DB.First(&admins, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get admins by id",
+		"admins":  admins,
+	})
+}
+
+func CreateAdminController(c echo.Context) error {
+	admins := model.Admin{}
+	c.Bind(&admins)
+
+	if err := config.DB.Save(&admins).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new admins",
+		"admins":  admins,
+	})
+}
+
+func UpdateAdminController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	var admins model.Admin
+
+	if err := config.DB.First(&admins, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+
+	c.Bind(&admins)
+
+	if err := config.DB.Save(&admins).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success update admin by id",
+		"admin":   admins,
+	})
+}
+
+func ValidateJobsController(c echo.Context) error {
+	return config.DB.Error
+}
