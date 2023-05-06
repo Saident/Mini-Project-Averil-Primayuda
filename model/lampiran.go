648aa495bcdfbd2c0e3390f6679c1da501bb237e
Saident <averilprimayuda@gmail.com>
2023-05-06 19:34:49 +0700

    Removed ID and references

--- model/lampiran.go
+++ model/lampiran.go
@@ -6,10 +6,8 @@ import (
 
 type Lampiran struct {
 	gorm.Model
-	ID               int    `gorm:"primaryKey"`
 	Lampiran_tipe    int    `json:"Lampiran_tipe" form:"Lampiran_tipe"`
 	Lampiran_content string `json:"Lampiran_content" form:"Lampiran_content"`
 
 	UserID int
-	User   User `gorm:"references:id"`
 }
