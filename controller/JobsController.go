2061d081fb0ad109f63d59586a73a8835a0f43b8
Saident <averilprimayuda@gmail.com>
2023-05-07 13:52:01 +0700

    Moved GetJwtClaims

--- controller/JobsController.go
+++ controller/JobsController.go
@@ -1,14 +1,10 @@
 package controller
 
 import (
-	"log"
 	"net/http"
-	"strings"
 
 	"github.com/Saident/Mini-Project-Averil-Primayuda/config"
-	"github.com/Saident/Mini-Project-Averil-Primayuda/constants"
 	"github.com/Saident/Mini-Project-Averil-Primayuda/model"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 )
 
@@ -29,30 +25,4 @@ func GetJobsController(c echo.Context) error {
 		})
 	}
 	return echo.ErrBadRequest
-}
-
-func GetJwtClaims(c echo.Context) (jwt.MapClaims, bool) {
-	authHeader := c.Request().Header.Get("Authorization")
-	authHeaderParts := strings.Split(authHeader, " ")
-	if len(authHeaderParts) != 2 || authHeaderParts[0] != "Bearer" {
-		return nil, false
-	}
-	jwtToken := authHeaderParts[1]
-
-	hmacSecretString := jwtToken
-	hmacSecret := []byte(constants.SECRET_JWT)
-	token, err := jwt.Parse(hmacSecretString, func(token *jwt.Token) (interface{}, error) {
-		return hmacSecret, nil
-	})
-
-	if err != nil {
-		return nil, false
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		return claims, true
-	} else {
-		log.Printf("Invalid JWT Token")
-		return nil, false
-	}
 }
