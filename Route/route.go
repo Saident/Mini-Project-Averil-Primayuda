23f0f6653eae4bf1140b0619d01c2591c7a21e9a
Saident <averilprimayuda@gmail.com>
2023-05-07 13:53:44 +0700

    Reworked Route

--- Route/route.go
+++ Route/route.go
@@ -12,19 +12,38 @@ import (
 func New() *echo.Echo {
 	e := echo.New()
 	//Non-JWT Route
+	//login
+	e.POST("/login/user", controller.LoginUser)
+	e.POST("/login/perusahaan", controller.LoginPerusahaan)
+	e.POST("/login/admin", controller.LoginAdmin)
+
+	//register
+	e.POST("/register", controller.CreateUserController)
+	e.POST("/register/perusahaan", controller.CreatePerusahaanController)
 
 	//Testing
 	e.GET("/users", controller.GetUsersController)
-	e.POST("/login", controller.LoginUser)
-	e.POST("/loginPerusahaan", controller.LoginPerusahaan)
-	e.POST("/register", controller.CreateUserController)
-	e.POST("/registerPerusahaan", controller.CreatePerusahaanController)
-	e.GET("/jobs", controller.GetJobsController)
-	e.POST("/postjobs", controller.PostJobsController)
+	e.GET("/perusahaan", controller.GetPerusahaansController)
 
+	//JWT Route
 	eJwt := e.Group("")
 	eJwt.Use(mid.JWT([]byte(constants.SECRET_JWT)))
-	//JWT Route
+
+	//TODO : add /update & /delete
+
+	//users
+	eJwt.GET("/jobs", controller.GetJobsController)
+
+	eJwt.POST("/lamaran/:perusahaan_id/:job_id", controller.PostLamaranController)
+	eJwt.GET("/lamaran/status", controller.GetLamaranStatusController)
+
+	//perusahaan
+	eJwt.POST("/jobs/post", controller.PostJobsController)
+	eJwt.GET("/jobs/perusahaan", controller.GetJobByPerusahaanController)
+
+	eJwt.GET("/lamaran/perusahaan", controller.GetAllLamaranByPerusahaanController)
+	eJwt.GET("/lamaran/perusahaan/:lamaran_id", controller.GetLamaranByIdController)
+	eJwt.POST("lamaran/perusahaan/validate/:lamaran_id", controller.ValidateLamaranController)
 
 	m.LogMiddleware(e)
 	return e
