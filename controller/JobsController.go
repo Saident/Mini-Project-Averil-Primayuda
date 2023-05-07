1e5ef01d9b95c800cb2a555cd1cf39fdf101e522
Saident <averilprimayuda@gmail.com>
2023-05-07 16:05:40 +0700

    Added GetJobByIdController

--- controller/JobsController.go
+++ controller/JobsController.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/Saident/Mini-Project-Averil-Primayuda/config"
 	"github.com/Saident/Mini-Project-Averil-Primayuda/model"
@@ -10,8 +11,8 @@ import (
 
 func GetJobsController(c echo.Context) error {
 	var jobs []model.Jobs
-	claims, err := GetJwtClaims(c)
-	if !err {
+	claims, bool := GetJwtClaims(c)
+	if !bool {
 		return echo.NewHTTPError(http.StatusBadRequest)
 	}
 	role := claims["role"].(string)
@@ -23,6 +24,32 @@ func GetJobsController(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"message": "success get all jobs",
 			"jobs":    jobs,
+		})
+	}
+	return echo.ErrBadRequest
+}
+
+func GetJobByIdController(c echo.Context) error {
+	var jobs []model.Jobs
+
+	job_id, err := strconv.Atoi(c.Param("job_id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	claims, bool := GetJwtClaims(c)
+	if !bool {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+	role := claims["role"].(string)
+	if role == "user" || role == "admin" {
+		if err := config.DB.First(&jobs, job_id).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success get job by id",
+			"job":     jobs,
 		})
 	}
 	return echo.ErrBadRequest
