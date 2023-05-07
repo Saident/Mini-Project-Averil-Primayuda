1161b443bd1f15ba9ed2dcab6c0b442af0b30b00
Saident <averilprimayuda@gmail.com>
2023-05-07 23:22:22 +0700

    Change on GetJwtClaims

--- controller/JobsController.go
+++ controller/JobsController.go
@@ -6,12 +6,14 @@ import (
 
 	"github.com/Saident/Mini-Project-Averil-Primayuda/config"
 	"github.com/Saident/Mini-Project-Averil-Primayuda/model"
+	"github.com/Saident/Mini-Project-Averil-Primayuda/utils"
+
 	"github.com/labstack/echo"
 )
 
 func GetJobsController(c echo.Context) error {
 	var jobs []model.Jobs
-	claims, bool := GetJwtClaims(c)
+	claims, bool := utils.GetJwtClaims(c)
 	if !bool {
 		return echo.NewHTTPError(http.StatusBadRequest)
 	}
@@ -37,7 +39,7 @@ func GetJobByIdController(c echo.Context) error {
 		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
 	}
 
-	claims, bool := GetJwtClaims(c)
+	claims, bool := utils.GetJwtClaims(c)
 	if !bool {
 		return echo.NewHTTPError(http.StatusBadRequest)
 	}
@@ -53,4 +55,85 @@ func GetJobByIdController(c echo.Context) error {
 		})
 	}
 	return echo.ErrBadRequest
+}
+
+func PostJobsController(c echo.Context) error {
+	jobs := model.Jobs{}
+
+	claims, err := utils.GetJwtClaims(c)
+	if !err {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+	role := claims["role"].(string)
+	perusahaan_id := claims["id"].(float64)
+
+	if role == "perusahaan" {
+		jobs.PerusahaanID = int(perusahaan_id)
+		jobs.Status = "Belum Divalidasi"
+		c.Bind(&jobs)
+
+		if err := config.DB.Save(&jobs).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success create new jobs",
+			"jobs":    jobs,
+		})
+	}
+	return echo.ErrForbidden
+}
+
+func GetJobByPerusahaanController(c echo.Context) error {
+	claims, err := utils.GetJwtClaims(c)
+	if !err {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+	role := claims["role"].(string)
+	perusahaan_id := claims["id"].(float64)
+
+	var jobs []model.Jobs
+	if role == "perusahaan" {
+		if err := config.DB.Where("perusahaan_id = ?", perusahaan_id).Find(&jobs).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success get jobs by perusahaan id",
+			"jobs":    jobs,
+		})
+	}
+	return echo.ErrForbidden
+}
+
+func UpdateJobByPerusahaanController(c echo.Context) error {
+	claims, bool := utils.GetJwtClaims(c)
+	if !bool {
+		return echo.NewHTTPError(http.StatusBadRequest, "messages: invalid JWT")
+	}
+	role := claims["role"].(string)
+	perusahaan_id := claims["id"].(float64)
+
+	job_id, err := strconv.Atoi(c.Param("job_id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	}
+
+	if role == "perusahaan" {
+		var jobs model.Jobs
+		if err := config.DB.Where("job_id = ? AND perusahaan_id = ?", job_id, perusahaan_id).First(&jobs).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		c.Bind(&jobs)
+
+		if err := config.DB.Save(&jobs).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success update job by id",
+			"job":     jobs,
+		})
+	}
+	return echo.ErrForbidden
 }
