3895dbc3fea8484fe2fb04776de2f4fb9133f506
Saident <averilprimayuda@gmail.com>
2023-05-07 13:53:15 +0700

    Added Lamaran Feature for Users

--- controller/UserController.go
+++ controller/UserController.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/Saident/Mini-Project-Averil-Primayuda/config"
 	"github.com/Saident/Mini-Project-Averil-Primayuda/model"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 )
 
@@ -21,22 +23,26 @@ func GetUsersController(c echo.Context) error {
 	})
 }
 
-//TODO : add get data from JWT, remove id
 func GetUserController(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
+	claims, bool := GetJwtClaims(c)
+	if !bool {
+		return echo.NewHTTPError(http.StatusBadRequest, "messages: invalid JWT")
 	}
+	role := claims["role"].(string)
+	user_id := claims["id"].(float64)
 
-	var users model.User
-	if err := config.DB.First(&users, id).Error; err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	if role == "user" {
+		var users model.User
+		if err := config.DB.First(&users, user_id).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success get users by id",
+			"users":   users,
+		})
 	}
-
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success get users by id",
-		"users":   users,
-	})
+	return echo.ErrForbidden
 }
 
 func CreateUserController(c echo.Context) error {
@@ -53,35 +59,97 @@ func CreateUserController(c echo.Context) error {
 }
 
 func UpdateUserController(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
+	claims, bool := GetJwtClaims(c)
+	if !bool {
+		return echo.NewHTTPError(http.StatusBadRequest, "messages: invalid JWT")
+	}
+	role := claims["role"].(string)
+	user_id := claims["id"].(float64)
+
+	if role == "user" {
+		var users model.User
+		if err := config.DB.First(&users, user_id).Error; err != nil {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		}
+
+		c.Bind(&users)
+
+		if err := config.DB.Save(&users).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success update user by id",
+			"user":    users,
+		})
+	}
+	return echo.ErrForbidden
+}
+
+func PostLamaranController(c echo.Context) error {
+	lamarans := model.Lamaran{}
+	perusahaan_id, err := strconv.Atoi(c.Param("perusahaan_id"))
 	if err != nil {
 		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
 	}
 
-	var users model.User
-
-	if err := config.DB.First(&users, id).Error; err != nil {
-		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	job_id, err := strconv.Atoi(c.Param("job_id"))
+	if err != nil {
+		echo.NewHTTPError(http.StatusBadRequest, "messages: invalid id parameter")
 	}
 
-	c.Bind(&users)
-
-	if err := config.DB.Save(&users).Error; err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	claims, bool := GetJwtClaims(c)
+	if !bool {
+		return echo.NewHTTPError(http.StatusBadRequest, "messages: invalid JWT")
 	}
+	role := claims["role"].(string)
+	user_id := claims["id"].(float64)
 
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success update user by id",
-		"user":    users,
-	})
-}
+	if role == "user" {
 
-func PostLamaranController(c echo.Context) error {
-	return config.DB.Error
+		if err := config.DB.Where("user_id = ? AND job_id = ?", user_id, job_id).First(&lamarans).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				lamarans.UserID = int(user_id)
+				lamarans.PerusahaanID = perusahaan_id
+				lamarans.JobID = job_id
+				c.Bind(&lamarans)
+
+				if err := config.DB.Save(&lamarans).Error; err != nil {
+					return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+				}
+				return c.JSON(http.StatusOK, map[string]interface{}{
+					"message":  "success create new lamarans",
+					"lamarans": lamarans,
+				})
+			} else {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+			}
+		} else {
+			return echo.NewHTTPError(http.StatusBadRequest, "You already Applied for the Job")
+		}
+	}
+	return echo.ErrForbidden
 }
 
 func GetLamaranStatusController(c echo.Context) error {
-	return config.DB.Error
+	claims, bool := GetJwtClaims(c)
+	if !bool {
+		return echo.NewHTTPError(http.StatusBadRequest, "messages: invalid JWT")
+	}
+	role := claims["role"].(string)
+	user_id := claims["id"].(float64)
+
+	if role == "user" {
+		var lamarans model.Lamaran
+		if err := config.DB.Where("user_id = ?", user_id).Find(&lamarans).Error; err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message":  "success get all lamarans",
+			"lamarans": lamarans,
+		})
+	}
+	return echo.ErrForbidden
 }
 
 func PostLampiranController(c echo.Context) error {
